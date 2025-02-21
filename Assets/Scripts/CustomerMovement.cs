402367c077656938a4520e5ac6453548ecd355d1
NoelStudios2017 <73044825+NoelStudios2017@users.noreply.github.com>
2025-02-21 21:51:55 +0000

    Changed the movement for customers.
    Changed how the customers move.

--- Assets/Scripts/CustomerMovement.cs
+++ Assets/Scripts/CustomerMovement.cs
@@ -1,86 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomerMovement : MonoBehaviour
 {
-    
-    Rigidbody2D custBody;
-    [SerializeField] float moveSpeed;
-    GameObject currentStartPoint;
-    [SerializeField] GameObject shoppingPointA;
-    [SerializeField] GameObject shoppingPointB;
-    [SerializeField] GameObject shoppingPointC;
-    [SerializeField] GameObject shoppingPointGettingServed;
-    [SerializeField] GameObject shopEntrance;
-    public bool isMoving;
-    public bool isServed;
-    int walkDirection;
-    public float walkTime;
-    float walkCounter;
-    public float waitTime;
-    float waitCounter;
+    Rigidbody2D rb;
+    public float speed;
+    public GameObject[] moveSpots;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitime;
+    public bool isServed = false;
+    public bool isMoving = false;
     private void Start()
     {
-        custBody=GetComponent<Rigidbody2D>();
-        waitCounter = waitTime;
-        walkCounter = walkTime;
-        Move();
-        currentStartPoint.transform.position = shopEntrance.transform.position;
-        //SetAnimator play idle or something?)
+       randomSpot=Random.Range(0,moveSpots.Length);
+       waitTime = startWaitime;
+       moveSpots = GameObject.FindGameObjectsWithTag("Positions"); 
+       isMoving = true;
+    rb = GetComponent<Rigidbody2D>();
     }
-
     private void Update()
     {
-        //Vector2 point =currentStartPoint.transform.position-t
         if(isMoving)
         {
-            walkCounter -= Time.deltaTime;
-            
-            switch (walkDirection)
-            {
-                case 0:
-                    Vector2.MoveTowards(shopEntrance.transform.position, shoppingPointA.transform.position, moveSpeed);
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].transform.position, speed * Time.deltaTime);
 
-                    //custBody.linearVelocity = new Vector2(0, moveSpeed);
-                    break;
-                case 1:
-                    //custBody.linearVelocity = new Vector2(moveSpeed, 0);
-                    break;
-                case 2:
-                    custBody.linearVelocity = new Vector2(0, -moveSpeed);
-                    break;
-                case 3:
-                    custBody.linearVelocity = new Vector2(-moveSpeed, 0);
-                    break;
-            }
-            if (walkCounter < 0)
+            if (Vector2.Distance(transform.position, moveSpots[randomSpot].transform.position) < 0.2f)
             {
-                isMoving = false;
-                waitCounter = waitTime;
+                if (waitTime <= 0)
+                {
+                    randomSpot = Random.Range(0, moveSpots.Length);
+                    waitTime = startWaitime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                    rb.bodyType = RigidbodyType2D.Kinematic;
+                }
             }
-        }
-        else
-        {
-            waitCounter -= Time.deltaTime;
-            custBody.linearVelocity = Vector2.zero;
-            if(waitCounter < 0)
-            {
-                Move();
-            }
-        }
-    }
-    public void Move()
-    {
-        //walkDirection = Random.Range(0, 4);
-        isMoving = true;
-        walkCounter=walkTime;
-
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("GettingServed"))
-        {
-            isMoving = false;
-            isServed = true;
         }
     }
 }
